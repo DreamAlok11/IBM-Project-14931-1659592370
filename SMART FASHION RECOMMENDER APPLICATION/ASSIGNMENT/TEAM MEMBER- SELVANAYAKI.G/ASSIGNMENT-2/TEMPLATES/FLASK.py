62c7eddb8cd7ec6f9ee3f4b2343f82da5695902d
Selvanayaki17 <108866269+Selvanayaki17@users.noreply.github.com>
2022-11-07 00:39:39 +0530

    Add files via upload

---
+++ SMART FASHION RECOMMENDER APPLICATION/ASSIGNMENT/TEAM MEMBER- SELVANAYAKI.G/ASSIGNMENT-2/TEMPLATES/FLASK.py
@@ -0,0 +1,24 @@
+# -- coding: utf-8 --
+"""
+Created on Sun Sep 25 16:53:16 2022
+@author: SELVI
+"""
+from flask import *
+
+app = Flask(_name_)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/signin')
+def sigin():
+    return render_template('signin.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
